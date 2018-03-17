caac8181fb6a9dd08918150d73fb94ae66d628a8
dom <soaresdominic@gmail.com>
2018-03-17 00:25:59 +0000

    initial commit

---
+++ src/MailboxInterface.java
@@ -0,0 +1,9 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface MailboxInterface extends Remote
+{
+	public Message receive (String agentname ) throws RemoteException ;
+	public void send(Message message) throws RemoteException;
+
+}
